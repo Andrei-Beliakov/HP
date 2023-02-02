8fe526cbf26b93be2887b22d1d8364e6d167ba90
Andrei Beliakov <andrei@imd.ru>
2023-02-02 19:06:43 +0300

    fix

--- script — копия.js
+++ script — копия.js
@@ -3,16 +3,16 @@ import { data } from "./data.js";
 const wrap = document.querySelector(".cards__wrapper");
 const inputSearch = document.querySelector(".form__input");
 const selectSearch = document.querySelector(".select");
-let selectArr = data;
+const selectArr = [];
 
-//ФУНКЦИЯ ОТРИСОВКИ КАРТОЧЕК
 function createCard(obj) {
   const newCard = document.createElement("div");
   newCard.className = "cards__item";
-  newCard.innerHTML = `<img src="${obj.image}" />`;
+  newCard.innerHTML = `
+  <img src="${obj.image}" />`;
   const text = document.createElement("div");
   text.className = "item__text";
-  text.innerHTML = `<p class="item__title">${obj.name}</p>
+  text.innerHTML = `  <p class="item__title">${obj.name}</p>
   <p class="item__data">Actor: ${obj.actor}</p>
   <p class="item__data">Gender: ${obj.gender}</p>
   <p class="item__data">House: ${obj.house}</p>
@@ -22,29 +22,30 @@ function createCard(obj) {
   return newCard;
 }
 
-//ВЫЗОВ ФУНКЦИИ ОТРИСОВКИ
-selectArr.forEach((card) => {
-  wrap.append(createCard(card));
-});
-
-//ПОИСК ПО INPUT
 function inputSearchFunction(event) {
   wrap.innerHTML = "";
   let value = event.target.value.trim().toLowerCase();
-  selectArr
+  data
     .filter((elem) => elem.name.trim().toLowerCase().includes(value))
     .forEach((elem) => wrap.append(createCard(elem)));
 }
+
 inputSearch.addEventListener("input", inputSearchFunction);
 
-//ПОИСК ПО SELECT
 function selectSearchFunction(event) {
   wrap.innerHTML = "";
   let value = event.target.value.trim().toLowerCase();
-  selectArr = data.filter((elem) =>
+  const selectArr = data.filter((elem) =>
     elem.house.trim().toLowerCase().includes(value)
   );
   console.log(selectArr);
-  selectArr.forEach((elem) => wrap.append(createCard(elem)));
+  data
+    .filter((elem) => elem.house.trim().toLowerCase().includes(value))
+    .forEach((elem) => wrap.append(createCard(elem)));
 }
+
 selectSearch.addEventListener("change", selectSearchFunction);
+
+data.forEach((card) => {
+  wrap.append(createCard(card));
+});
