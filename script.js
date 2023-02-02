e477c9c8764532752b3fbc0835ff4d1365bb7618
Andrei Beliakov <andrei@imd.ru>
2023-02-02 15:03:53 +0300

    made search and input

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ import { data } from "./data.js";
 const wrap = document.querySelector(".cards__wrapper");
 const inputSearch = document.querySelector(".form__input");
 const selectSearch = document.querySelector(".select");
-console.log(inputSearch);
-console.log(selectSearch);
 
 function createCard(obj) {
   const newCard = document.createElement("div");
@@ -21,42 +19,24 @@ function createCard(obj) {
   return newCard;
 }
 
-// function searchFunction(event) {
-//   wrap.innerHTML = "";
-//   console.log(event.target.value);
-//   let value = event.target.value.trim().toLowerCase();
-//   data
-//     .filter((elem) => elem.name.trim().toLowerCase().includes(value))
-//     .forEach((elem) => wrap.append(createCard(elem)));
-// }
-// search.addEventListener("input", searchFunction);
+function inputSearchFunction(event) {
+  wrap.innerHTML = "";
+  console.log(event.target.value);
+  let value = event.target.value.trim().toLowerCase();
+  data
+    .filter((elem) => elem.name.trim().toLowerCase().includes(value))
+    .forEach((elem) => wrap.append(createCard(elem)));
+}
+inputSearch.addEventListener("input", inputSearchFunction);
+
+function selectSearchFunction(event) {
+  wrap.innerHTML = "";
+  console.log(event.target.value);
+  let value = event.target.value.trim().toLowerCase();
+  data
+    .filter((elem) => elem.house.trim().toLowerCase().includes(value))
+    .forEach((elem) => wrap.append(createCard(elem)));
+}
+selectSearch.addEventListener("input", selectSearchFunction);
 
 data.forEach((card) => wrap.append(createCard(card)));
-
-// const wrap = document.querySelector(".cards__wrapper");
-// const search = document.querySelector(".cards__input");
-
-// function createCard(obj) {
-//   let checkedKeywords = [...new Set(obj.keywords.split(" "))].join(" ");
-//   const newCard = document.createElement("div");
-//   newCard.className = "cards__item";
-//   newCard.innerHTML = `<p class="item__emoji">${obj.symbol}</p>
-//      <p class="item__title">${obj.title}</p>
-//      <p class="item__keywords">${checkedKeywords}</p>`;
-//   return newCard;
-// }
-
-// function searchFunction(event) {
-//   wrap.innerHTML = "";
-//   console.log(event.target.value);
-//   let value = event.target.value.trim().toLowerCase();
-//   data
-//     .filter(
-//       (elem) =>
-//         elem.title.trim().toLowerCase().includes(value) ||
-//         elem.keywords.trim().toLowerCase().includes(value)
-//     )
-//     .forEach((elem) => wrap.append(createCard(elem)));
-// }
-// search.addEventListener("input", searchFunction);
-// data.forEach((newCard) => wrap.append(createCard(newCard)));
