34acd3ea9ff582246ee60c206993f1e8bf4e01c0
Andrei Beliakov <andrei@imd.ru>
2023-02-02 11:27:19 +0300

    go on

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 import { data } from "./data.js";
 
 const wrap = document.querySelector(".cards__wrapper");
+const inputSearch = document.querySelector(".form__input");
+const selectSearch = document.querySelector(".select");
+console.log(inputSearch);
+console.log(selectSearch);
 
 function createCard(obj) {
-  console.log(obj.wand);
   const newCard = document.createElement("div");
   newCard.className = "cards__item";
   newCard.innerHTML = `
-  <img src="${obj.image}"/> 
+  <img src="${obj.image}" />
   <p class="item__title">${obj.name}</p>
   <p class="item__data">Actor: ${obj.actor}</p>
-  <p class="item__data">Gender:${obj.gender}</p>
-  <p class="item__data">House:${obj.house}</p>
-  <p class="item__data">Wand core:${obj.wand}</p>
-  <p class="item__data">Alive:${obj.alive}</p>`;
+  <p class="item__data">Gender: ${obj.gender}</p>
+  <p class="item__data">House: ${obj.house}</p>
+  <p class="item__data">Wand core: ${obj.wand.core}</p>
+  <p class="item__data">Alive: ${obj.alive ? "yes" : "no"}</p>`;
 
   return newCard;
 }
 
+// function searchFunction(event) {
+//   wrap.innerHTML = "";
+//   console.log(event.target.value);
+//   let value = event.target.value.trim().toLowerCase();
+//   data
+//     .filter((elem) => elem.name.trim().toLowerCase().includes(value))
+//     .forEach((elem) => wrap.append(createCard(elem)));
+// }
+// search.addEventListener("input", searchFunction);
+
 data.forEach((card) => wrap.append(createCard(card)));
 
-// import { data } from "./data.js";
 // const wrap = document.querySelector(".cards__wrapper");
 // const search = document.querySelector(".cards__input");
 
